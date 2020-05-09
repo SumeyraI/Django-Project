8d523d2517475ce4b377e44fa5a69cb725e0a34f
SumeyraI <sumeyraisik34@gmail.com>
2020-05-10 01:49:52 +0300

    26.video uye paneli ve Profil bilgileri yapıldı.

--- project1/urls.py
+++ project1/urls.py
@@ -27,6 +27,7 @@ urlpatterns = [
     path('iletisim/', views.iletisim, name='iletisim'),
     path('home/', include('home.urls')),
     path('food/', include('food.urls')),
+    path('user/', include('user.urls')),
     path('admin/', admin.site.urls),
     path('ckeditor/', include('ckeditor_uploader.urls')),
     path('category/<int:id>/<slug:slug>/', views.category_products, name='category_products'),
