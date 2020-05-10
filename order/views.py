eb35fd3b6ba0bebb4ba0ff31ec5c1d3c13c2e9b3
SumeyraI <sumeyraisik34@gmail.com>
2020-05-10 22:41:22 +0300

    30.video siparis yapma ve tamamlama yapıldı.

--- order/views.py
+++ order/views.py
@@ -4,8 +4,11 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
-from food.models import Category
-from order.models import ShopCartForm, ShopCart
+from django.utils.crypto import get_random_string
+
+from food.models import Category, Food
+from home.models import UserProfile
+from order.models import ShopCartForm, ShopCart, OrderForm, Order, OrderProduct
 
 
 def index(request):
@@ -86,3 +89,70 @@ def deletefromcart(request,id):
     request.session['cart_items'] = ShopCart.objects.filter(user_id=current_user.id).count()
     messages.success(request, "Ürün sepetten silinmiştir.")
     return HttpResponseRedirect("/shopcart")
+
+
+
+@login_required(login_url='/login')
+def orderproduct(request):  #siparis edilen urunu kaydediyoruz.
+    current_user = request.user
+    category = Category.objects.all()
+    shopcart = ShopCart.objects.filter(user_id=current_user.id)
+    total=0
+    for rs in shopcart:
+        total += rs.quantity * rs.product.price
+
+
+    if request.method == 'POST':
+        form = OrderForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+
+            # Send Credit card information to bank and get result
+            # If payment accepted continue else send payment error to checkout page
+
+            data = Order()
+            data.first_name = form.cleaned_data['first_name'] #get product quantity from form
+            data.last_name = form.cleaned_data['last_name']
+            data.address = form.cleaned_data['address']
+            data.city = form.cleaned_data['city']
+            data.phone = form.cleaned_data['phone']
+            data.user_id = current_user.id
+            data.total = total
+            data.ip = request.META.get('REMOTE_ADDR')
+            ordercode = get_random_string(5).upper()
+            data.code = ordercode
+            data.save()
+
+            shopcart = ShopCart.objects.filter(user_id=current_user.id)
+            for rs in shopcart:
+                detail = OrderProduct()
+                detail.order_id     = data.id # Order Id
+                detail.product_id   = rs.product_id
+                detail.user_id      = current_user.id
+                detail.quantity     = rs.quantity
+                #  Reduce product Amount  (quantity)
+                detail.price = rs.product.price
+                detail.amount = rs.amount
+                detail.save()
+
+                product = Food.objects.get(id=rs.product_id)
+                product.amount -= rs.quantity #urunu stoktan sayısını azalttık
+                product.save()
+
+            ShopCart.objects.filter(user_id=current_user.id).delete() # Clear & Delete shopcart
+            request.session['cart_items']=0
+            messages.success(request, "Your Order has been completed. Thank You ")
+            return render(request, 'Order_Completed.html',{'ordercode':ordercode,'category':category})
+        else:
+            messages.warning(request, form.errors)
+            return HttpResponseRedirect("/order/orderproduct")
+
+    form = OrderForm()
+    profile = UserProfile.objects.get(user_id=current_user.id)
+    context = {'form': form,
+               'category':category,
+               'shopcart': shopcart,
+               'total': total,
+               'profile':profile,
+               }
+    return render(request, 'Order_Form.html', context)
