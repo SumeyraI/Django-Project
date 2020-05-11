b7b70434e22ade3c7159b7c5e74b18b56e977969
SumeyraI <sumeyraisik34@gmail.com>
2020-05-11 21:03:04 +0300

    32.video uye siparis listesi siparis detayi yapıldı.

--- user/views.py
+++ user/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import PasswordChangeForm
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, redirect
@@ -7,9 +8,10 @@ from django.contrib import messages
 # Create your views here.
 from food.models import Category
 from home.models import UserProfile
+from order.models import Order, OrderProduct
 from user.forms import UserUpdateForm, ProfileUpdateForm
 
-
+@login_required(login_url='/login')
 def index(request):
     category = Category.objects.all()
     current_user = request.user
@@ -23,6 +25,7 @@ def index(request):
 
     return render(request, 'user_profile.html', context)
 
+@login_required(login_url='/login')
 def user_update(request):
     if request.method == 'POST':
         user_form = UserUpdateForm(request.POST, instance=request.user)
@@ -45,6 +48,7 @@ def user_update(request):
         }
         return render(request, 'user_update.html', context)
 
+@login_required(login_url='/login')
 def change_password(request):
     if request.method == 'POST':
         form = PasswordChangeForm(request.user, request.POST)
@@ -62,3 +66,27 @@ def change_password(request):
         return render(request, 'change_password.html', {
         'form': form, 'category': category
     })
+
+@login_required(login_url='/login')
+def orders(request):
+    category = Category.objects.all()
+    current_user = request.user
+    orders = Order.objects.filter(user_id=current_user.id)
+    context = {
+        'category': category,
+        'orders': orders,
+    }
+    return render(request, 'user_orders.html', context)
+
+@login_required(login_url='/login')
+def orderdetail(request,id):
+    category = Category.objects.all()
+    current_user = request.user
+    order = Order.objects.get(user_id=current_user.id, id=id)
+    orderitems = OrderProduct.objects.filter(order_id=id)
+    context = {
+        'category': category,
+        'order': order,
+        'orderitems': orderitems,
+    }
+    return render(request, 'user_order_detail.html', context)
